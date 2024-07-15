3a41221f20221e8b6b8105233e9b04f933126d1c
Maks <krivorukov2007@mail.ru>
2024-07-15 18:04:14 +0300

    fixed error with bad user password

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -63,6 +63,7 @@ const AuthProvider : FC<PropsWithChildren> = ({children}) => {
             setIsLoading(false)
             
         } catch(error){
+            setUser(null)
             console.log(error)
             
         }
